59caae3fedbc00e4bedc66b09140afac5f392ae6
Viktor Baranov <baranov71viktor@gmail.com>
2025-04-26 21:03:28 +0300

    feat: new structure

---
+++ config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false, -- Show dotfiles
+        hide_gitignored = false, -- Show gitignored files
+      },
+    },
+  },
+}
